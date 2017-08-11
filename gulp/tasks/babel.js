fd0be3cae7fa3db68aa6ca20e4dea6bd75bc70ea
SparkingR <sparkurumi@gmail.com>
2017-08-11 17:50:49 +0800

    add order to babel task

--- gulp/tasks/babel.js
+++ gulp/tasks/babel.js
@@ -2,6 +2,7 @@ const gulp          = require('gulp');
 const plumber       = require('gulp-plumber');
 const sourcemaps    = require('gulp-sourcemaps');
 const babel         = require('gulp-babel');
+const order         = require('gulp-order');
 const concat        = require('gulp-concat');
 const config        = require('../gulpconfig');
 
@@ -10,11 +11,17 @@ const plugins = {
     presets: ['es2015']
 }
 
+const fileOrder = [
+    `${config.path.dev.js}/b.js`,
+    `${config.path.dev.js}/a.js`
+]
+
 gulp.task('babel', () =>
     gulp.src(`${config.path.dev.js}/*.js`)
         .pipe(plumber())
         .pipe(sourcemaps.init())
         .pipe(babel(plugins))
+        .pipe(order(fileOrder, {base: './'}))
         .pipe(concat('all.js'))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(config.path.dist.js))
